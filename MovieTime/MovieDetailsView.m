a79684a386ed431526e7ce6652e0ac2c48b9e281
Stefan Buzoianu <mariusjcb@Mariuss-MacBook-Pro.local>
2016-09-08 18:12:01 +0300

    MovieDetails Design (v2)

--- MovieTime/MovieDetailsView.m
+++ MovieTime/MovieDetailsView.m
@@ -39,7 +39,7 @@
     UIView *header = [[UIView alloc] initWithFrame:CGRectMake(-borderWidth, -navBarHeight-borderWidth, frameWidth+borderWidth, 250+navBarHeight+borderWidth*2)];
     header.backgroundColor = [UIColor lightGrayColor];
     
-    UIImageView *backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, header.frame.size.height)];
+    UIImageView *backgroundImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, frameWidth, header.frame.size.height)];
     
     backgroundImage.image = image;
     backgroundImage.contentMode = UIViewContentModeScaleAspectFill;
@@ -54,36 +54,47 @@
     header.layer.shadowOffset = CGSizeMake(0, 2);
     header.layer.shadowOpacity = 0.15;
     
+    UIScrollView *moviesList = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, frameWidth, header.frame.size.height)];
+    
     //MARK: COVER_IMG1
     
-    UIImageView *moviesList = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.frame.size.width, header.frame.size.height)];
+    UIView *coverImgView = [[UIView alloc] initWithFrame:CGRectMake((frameWidth-155)/2, navBarHeight+20, 155, backgroundImage.frame.size.height - 40 - navBarHeight)];
     
-    UIImageView *coverImage = [[UIImageView alloc] initWithFrame:CGRectMake((frameWidth-155)/2, navBarHeight+20, 155, backgroundImage.frame.size.height - 40 - navBarHeight)];
+    UIImageView *coverImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, coverImgView.frame.size.width, coverImgView.frame.size.height)];
+    
     coverImage.image = image;
     coverImage.contentMode = UIViewContentModeScaleAspectFill;
+    
     [coverImage setClipsToBounds:YES];
     
-    coverImage.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7].CGColor;
-    coverImage.layer.borderWidth = borderWidth;
-    coverImage.layer.shadowColor = [UIColor blackColor].CGColor;
-    coverImage.layer.shadowOffset = CGSizeMake(0, 4);
-    coverImage.layer.shadowOpacity = 0.4;
+    coverImgView.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:0.8f].CGColor;
+    coverImgView.layer.borderWidth = borderWidth;
+    coverImgView.layer.shadowColor = [UIColor blackColor].CGColor;
+    coverImgView.layer.shadowOffset = CGSizeMake(0, 4);
+    coverImgView.layer.shadowOpacity = 0.4;
+    
+    [coverImgView addSubview:coverImage];
+    [moviesList addSubview:coverImgView];
     
     //MARK: COVER_IMG2
     
-    UIImageView *coverImage2 = [[UIImageView alloc] initWithFrame:CGRectMake(coverImage.frame.origin.x+coverImage.frame.size.width+20, navBarHeight+20, 155, backgroundImage.frame.size.height - 40 - navBarHeight)];
+    UIView *coverImgView2 = [[UIView alloc] initWithFrame:CGRectMake(coverImgView.frame.origin.x+coverImgView.frame.size.width+20, navBarHeight+20, 155, backgroundImage.frame.size.height - 40 - navBarHeight)];
+    
+    UIImageView *coverImage2 = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, coverImgView2.frame.size.width, coverImgView2.frame.size.height)];
+    
     coverImage2.image = image;
     coverImage2.contentMode = UIViewContentModeScaleAspectFill;
+    
     [coverImage2 setClipsToBounds:YES];
     
-    coverImage2.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7].CGColor;
-    coverImage2.layer.borderWidth = borderWidth;
-    coverImage2.alpha = 0.55f;
+    coverImgView2.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:0.8f].CGColor;
+    coverImgView2.layer.borderWidth = borderWidth;
+    coverImgView2.alpha = 0.55f;
+    
+    [coverImgView2 addSubview:coverImage2];
+    [moviesList addSubview:coverImgView2];
     
     //MARK: ADD SUBVIEWS
-    
-    [moviesList addSubview:coverImage];
-    [moviesList addSubview:coverImage2];
     
     [backgroundImage addSubview:moviesList];
     [header addSubview:backgroundImage];
